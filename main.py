6193a0d6edd6725b1c1a80c9b3a422bfd2a3fa44
Stanley Otieno <stanlieds@gmail.com>
2024-06-14 22:49:36 +0300

    make changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
+from typing import Optional
 
 
 app = FastAPI()
@@ -11,11 +12,25 @@ def index():
 
 
 
-@app.get("/about")
-def about():
-    return {"data": "About page"}
+@app.get("/blog")
+def about(limit=10, published: bool = True, sort: Optional[str]= None):
+    if published:
+        return {"data": f'{limit} published blogs from the db'}
+    else:
+        return {"data": f'{limit} blogs from the db'}
 
 
-@app.get("/blog/id")
-def show():
-    return {"data": "Blog page"}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get("/blog/{id}")
+def show(id: int):
+    return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id):
+    return {"data": {'1','2'}}
