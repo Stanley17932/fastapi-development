385f4aac7499c14a13e237fa314cbc70e19baf49
Stanley Otieno <stanlieds@gmail.com>
2024-06-14 23:32:09 +0300

    add changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from typing import Optional
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -26,11 +26,23 @@ def unpublished():
     return {'data': 'all unpublished blogs'}
 
 
+
+
 @app.get("/blog/{id}")
 def show(id: int):
     return {"data": id}
 
 
 @app.get("/blog/{id}/comments")
-def comments(id):
+def comments(id, limit=10):
     return {"data": {'1','2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post("/blog")
+def create_blog(blog: Blog):
+    return {'data': f'blog is created with as {blog.title}'}
